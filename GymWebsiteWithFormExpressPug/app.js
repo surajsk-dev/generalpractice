6cd71ae0e3eb156217a237444620d9759309d6bb
suraj soan kamble <35141388+surajsoankamble@users.noreply.github.com>
2021-05-04 17:22:04 +0530

    tut 74 finished Gym site with backend
    Finished Gym site with backend storing form data in text file using fs and encoded url

--- GymWebsiteWithFormExpressPug/app.js
+++ GymWebsiteWithFormExpressPug/app.js
@@ -1,12 +1,22 @@
 // import express
 const express=require('express');
+
+// import fs to store form data in file
+const fs=require('fs');
+
 // creating express app
 const app=express();
+
+// to get form data we have use encoded url.
+app.use(express.urlencoded());
+
 // import path for pug
 const path=require('path');
 
 // port number
 const port=80;
+
+
 
 // using static 
 app.use('/static',express.static('static'));
@@ -22,6 +32,25 @@ app.set('view engine','pug');
 app.get('/',(req,res)=>{
     const con='this is best content'
     const params={'title':'Gym Website','content':con}
+    res.status(200).render('index.pug',params);
+})
+
+
+// sending form data to file
+app.post('/',(req,res)=>{
+    // get form data and console
+    // data=req.body
+    // console.log(req.body)
+
+    // covert form data to string
+    let name=req.body.name;
+    let age=req.body.age;
+    let gender=req.body.gender;
+    let about=req.body.about;
+    let address=req.body.address;
+    outputData=`My name is ${name},${age} years old and ${gender},about me ${about}. My address is ${address} `
+    fs.writeFileSync('dataFile.txt',outputData)
+    const params={'title':'Gym Website'}
     res.status(200).render('index.pug',params);
 })
 
